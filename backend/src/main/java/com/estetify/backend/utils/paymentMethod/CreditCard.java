29728f3798e5acc266dff0f3018ecbe4eb1183f9
NatanNMendes <natan_nunes_mendes@outlook.com>
2025-01-29 11:35:33 -0300

    Adicionando o front end

--- backend/src/main/java/com/estetify/backend/utils/paymentMethod/CreditCard.java
+++ backend/src/main/java/com/estetify/backend/utils/paymentMethod/CreditCard.java
@@ -1,4 +1,69 @@
 package com.estetify.backend.utils.paymentMethod;
 
-public class CreditCard {
+import java.util.Date;
+
+public class CreditCard{
+    private String cardNumber;
+    private String cardholderName;
+    private Date expirationDate;
+    private String cvv;
+    private String installments;
+    private String cardBrand;
+
+    public CreditCard(String cardNumber, String cardholderName, Date expirationDate, String cvv, String installments, String cardBrand) {
+        this.cardNumber = cardNumber;
+        this.cardholderName = cardholderName;
+        this.expirationDate = expirationDate;
+        this.cvv = cvv;
+        this.installments = installments;
+        this.cardBrand = cardBrand;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public String getCardholderName() {
+        return cardholderName;
+    }
+
+    public void setCardholderName(String cardholderName) {
+        this.cardholderName = cardholderName;
+    }
+
+    public Date getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(Date expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public String getCvv() {
+        return cvv;
+    }
+
+    public void setCvv(String cvv) {
+        this.cvv = cvv;
+    }
+
+    public String getInstallments() {
+        return installments;
+    }
+
+    public void setInstallments(String installments) {
+        this.installments = installments;
+    }
+
+    public String getCardBrand() {
+        return cardBrand;
+    }
+
+    public void setCardBrand(String cardBrand) {
+        this.cardBrand = cardBrand;
+    }
 }
